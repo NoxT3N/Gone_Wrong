4bf4fe0da8240a789e34b61bd2e37c0a8116f9c8
Felix <145358483+NoxT3N@users.noreply.github.com>
2024-11-10 18:21:37 +0000

    Basic Character Controller 🚧
    with first person view and basic player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController player;
+
+    public float speed = 12f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        player.Move(move * speed * Time.deltaTime);
+    }
+}

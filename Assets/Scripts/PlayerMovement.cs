5c05c9e5baa1c764565b24b6e8e5a3330bea39d4
Felix <145358483+NoxT3N@users.noreply.github.com>
2024-11-10 19:50:05 +0000

    The player can now fall 🔨

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,12 +7,16 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 12f;
 
     const float gravity = -9.81f;
-
     Vector3 velocity;
 
     // Update is called once per frame
     void Update()
     {
+        if (player.isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 

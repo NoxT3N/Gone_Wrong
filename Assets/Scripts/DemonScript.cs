ac445a7181b0ab109b83f268f4885274f6ebb341
hannah-nanadev <hannahkellett2@gmail.com>
2024-11-13 00:36:22 +0000

    🚧 Demon teleports now!
    Still needs more rhyme or reason to it, polish + teleports out of justin's fov

--- Assets/Scripts/DemonScript.cs
+++ Assets/Scripts/DemonScript.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -5,19 +6,59 @@ public class DemonScript : MonoBehaviour
 {
 
     public GameObject demon;
+    public GameObject player;
     public int aggro;
 
     public MeshRenderer demonhider;
+    private bool isVisible = false;
+
+    
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        demonhider.enabled = false;
+        setVisible(isVisible);
+        StartCoroutine(Teleport());
     }
 
-    // FixedUpdate is like Update but not tied to framerate
-    void FixedUpdate()
+    // Update is called every frame
+    void Update()
     {
-        
+
+    }
+
+    //Hides demon + disables collision
+    void toggleVisible()
+    {
+        isVisible = !isVisible;
+        demonhider.enabled = isVisible;
+    }
+    void setVisible(bool visible)
+    {
+        demonhider.enabled = visible;
+    }
+
+    //Teleport and activate demon
+    IEnumerator Teleport()
+    {
+        Debug.Log("Demon teleportation coroutine started");
+        while (true)
+        {
+            yield return new WaitForSecondsRealtime(20);
+
+            Vector3 playerPos = player.transform.position;
+            Vector3 newPos = new Vector3();
+
+            newPos.x = playerPos.x + Random.Range(-20.0f, 20.0f);
+            newPos.y = playerPos.y;
+            newPos.x = playerPos.x + Random.Range(-20.0f, 20.0f);
+
+            demon.transform.position = newPos;
+            toggleVisible();
+            Debug.Log("Demon appeared!");
+            yield return new WaitForSecondsRealtime(5);
+            toggleVisible();
+            Debug.Log("Demon is gone.");
+        }
     }
 }

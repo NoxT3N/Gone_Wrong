1ceebf102ff73ed8e7a12911155cbfc01bd1d222
hannah-nanadev <hannahkellett2@gmail.com>
2024-11-13 23:46:06 +0000

    🚧 Demon teleports around you
    Does not teleport specifically behind you but it teleports around on the new navmesh! very cool! Made player's colour blue as  well

--- Assets/Scripts/DemonScript.cs
+++ Assets/Scripts/DemonScript.cs
@@ -1,5 +1,9 @@
+using System;
 using System.Collections;
+using System.Runtime.CompilerServices;
+using UnityEditor.PackageManager;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.UIElements;
 
 public class DemonScript : MonoBehaviour
@@ -7,11 +11,12 @@ public class DemonScript : MonoBehaviour
     [Header("Demon Settings")]
     [SerializeField] private GameObject player;
     [SerializeField] private int aggro;
+    private NavMeshAgent agent;
 
     void Start()
     {
-        //setVisible(isVisible);
-        StartCoroutine(Teleport());
+        agent = GetComponent<NavMeshAgent>();
+        StartCoroutine(Teleport());        
     }
 
 
@@ -32,21 +37,34 @@ public class DemonScript : MonoBehaviour
         Debug.Log("Demon teleportation coroutine started");
         while (true)
         {
-            yield return new WaitForSecondsRealtime(20);
+            yield return new WaitForSecondsRealtime(2);
 
             Vector3 playerPos = player.transform.position;
-            Vector3 newPos = new Vector3();
 
-            newPos.x = playerPos.x + Random.Range(-20.0f, 20.0f);
-            newPos.y = playerPos.y;
-            newPos.x = playerPos.x + Random.Range(-20.0f, 20.0f);
+            int xSide = UnityEngine.Random.Range(0, 2);
+            int zSide = UnityEngine.Random.Range(0, 2);
 
-            this.transform.position = newPos;
-            //toggleVisible();
-            Debug.Log("Demon appeared!");
-            yield return new WaitForSecondsRealtime(5);
-            //toggleVisible();
-            Debug.Log("Demon is gone.");
+            float xRange = UnityEngine.Random.Range(0.0f, 4.0f);
+            float zRange = UnityEngine.Random.Range(0.0f, 4.0f);
+
+            if(xSide == 0) xRange = xRange * -1;
+            if(zSide == 0) zRange = zRange * -1;
+
+            Vector3 randomPos = new Vector3(xRange+playerPos.x, playerPos.y, zRange+playerPos.z);
+
+            //INTRODUCING NEW NAVMESH TECHNOLOGY
+            NavMeshHit hit;
+            if(NavMesh.SamplePosition(randomPos, out hit, 4.0f, NavMesh.AllAreas))
+            {
+                agent.Warp(hit.position);
+                Debug.Log("Demon teleported!");
+                yield return new WaitForSecondsRealtime(2);
+            }
+            else
+            {
+                Debug.Log("Demon failed to find NavMesh around player. Teleportation failed.");
+            }
+            
         }
     }
 }

cdebe187607f16166ea7bbfd8a752fa88a1d6b1e
hannah-nanadev <hannahkellett2@gmail.com>
2024-12-16 14:58:29 +0000

    🚧 pieces lock into place but they don't snap to their correct position yet, unsure how to fix
    i'm just gonna work on the gathering pieces side of things for now instead

--- Assets/Scripts/PuzzlePiece.cs
+++ Assets/Scripts/PuzzlePiece.cs
@@ -8,19 +8,47 @@ public class PuzzlePiece : MonoBehaviour
     [SerializeField]
     private Canvas canvas;
 
-    public void DragHandler(BaseEventData data)
+    [SerializeField]
+    private GameObject intendedSpot;
+
+    [SerializeField]
+    private float lockTolerance;
+
+    private bool inSpot;
+
+    public void DragPiece(BaseEventData data)
     {
-        PointerEventData pData = (PointerEventData)data;
+        if (!inSpot)
+        {
+            PointerEventData pData = (PointerEventData)data;
 
-        Vector2 pos;
-        RectTransformUtility.ScreenPointToLocalPointInRectangle(
-            (RectTransform)canvas.transform,
-            pData.position,
-            canvas.worldCamera,
-            out pos
-            );
+            Vector2 pos;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                (RectTransform)canvas.transform,
+                pData.position,
+                canvas.worldCamera,
+                out pos
+                );
 
-        transform.position = canvas.transform.TransformPoint(pos);
+            transform.position = canvas.transform.TransformPoint(pos);
+        }
     }
 
+    public void DropPiece()
+    {
+        float distance = Vector2.Distance(transform.position, intendedSpot.transform.position);
+        if(distance <= lockTolerance)
+        {
+            inSpot = true;
+            Vector2 newPos;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                (RectTransform)canvas.transform,
+                (Vector2)intendedSpot.transform.position,
+                canvas.worldCamera,
+                out newPos
+                );
+
+            transform.position = canvas.transform.TransformPoint(newPos);
+        }
+    }
 }
